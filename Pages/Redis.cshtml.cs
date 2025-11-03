b0461560a9d81d640fbc35806d5786582c17a27b
Bartek <kierunb@gmail.com>
2025-11-03 10:36:33 +0100

    initial commit

---
+++ Pages/Redis.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StackExchange.Redis;
+namespace k8s_lab.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    private IConnectionMultiplexer? _connectionMultiplexer;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task OnGetAsync()
+    {
+        string? fooResult;
+
+        try
+        {
+            string connectionString = "redis:6379";
+            _connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(connectionString);
+            var db = _connectionMultiplexer.GetDatabase();
+            await db.StringSetAsync("date", DateTime.Now.ToString());
+            fooResult = await db.StringGetAsync("date");
+        }
+        catch (System.Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while accessing Redis");
+            throw;
+        }
+
+        ViewData["RedisDate"] = fooResult ?? "No value found";
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Demo App
+
+## VS Code Extensions
+
+- Azure Tools
+- Container Tools
+- Kubernetes
+- Azure Kubernetes Service
+
+## Other tools
+
+- [Azure CLI](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-windows?view=azure-cli-latest&pivots=winget)
+- [k9s](https://github.com/derailed/k9s/releases/tag/v0.50.16)
+- [LazyDocker](https://github.com/jesseduffield/lazydocker/releases/tag/v0.24.1)
+
+## Application
+
+- http://localhost:5154
